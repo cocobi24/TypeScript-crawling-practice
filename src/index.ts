b73ba24b48091a032c3353edbefc5e8c10c49080
hojin <cacabi24@hanmail.net>
2023-03-15 09:21:42 +0900

    👨‍💻 이미지 크롤링 및 다운로드
    👨‍💻 이미지 크롤링 및 다운로드

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
@@ -5,7 +7,6 @@ export class CapitalCityScraper {
   async scrapeCity(url: string) {
     const response = await axios.get(url);
     const html = response.data;
-
     const $ = cheerio.load(html);
 
     const cityName = $('#firstHeading').text().trim();
@@ -23,8 +24,38 @@ export class CapitalCityScraper {
     const populationText = populationRows.find('th:contains(Capital city) + td').text().trim();
     const population = parseFloat(populationText.replace(/,/g, ''));
     console.log(population);
+
+    const flagPageLink = $('.mergedtoprow a.image + div:contains(Flag)').prev().attr('href')!;
+    const flagPageUrl = new URL(flagPageLink, url).toString();
+    const flagImagePath = await this.scrapeImage(flagPageUrl);
+    console.log(flagImagePath);
   }
 
+  protected async scrapeImage(url: string) {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const imageLink = $('#file a').attr('href')!;
+    const imageUrl = new URL(imageLink, url).toString();
+
+    const imagePath = await this.downloadFile(imageUrl, 'flags');
+
+    return imagePath;
+  }
+
+  protected async downloadFile(url: string, dir: string) {
+    const response = await axios.get(url, {
+        responseType: 'arraybuffer'
+    });
+
+    fs.mkdirSync(dir, {recursive: true});
+
+    const filePath = path.join(dir, path.basename(url));
+    fs.writeFileSync(filePath, response.data);
+
+    return filePath;
+  }
 }
 
 async function main() {
